64be343c620d0520df6b9e8dc1f07107518df64f
CodeSandbox <support@codesandbox.io>
2023-08-03 20:15:43 +0000

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <img src="./Trollface.png.webp" alt="MyImage" />
+        <p>Meme Generator</p>
+      </header>
+    </>
+  );
+};
+export default Header;
